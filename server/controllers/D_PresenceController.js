aa9aff7ab371a00121542455f8c8a382058e745c
muhammad-raihan-i <raihan.indrastoto@gmail.com>
2025-09-22 16:25:10 +0700

    chore: routes all done

--- server/controllers/D_PresenceController.js
+++ server/controllers/D_PresenceController.js
@@ -1,27 +1,54 @@
 module.exports = class PresenceController {
   static async create(req, res, next) {
     try {
-      console.log("try at CompanyController create");
+      console.log("try at PresenceController create");
     } catch (error) {
-      console.log("error at CompanyController create");
+      console.log("error at PresenceController create");
+      console.log(error);
+      next(error);
+    }
+  }
+  static async findOne(req, res, next) {
+    try {
+      console.log("try PresenceController findOne");
+    } catch (error) {
+      console.log("error at PresenceController findOne");
+      console.log(error);
+      next(error);
+    }
+  }
+  static async findAll(req, res, next) {
+    try {
+      console.log("try PresenceController findAll");
+    } catch (error) {
+      console.log("error at PresenceController findAll");
+      console.log(error);
+      next(error);
+    }
+  }
+  static async findMine(req, res, next) {
+    try {
+      console.log("try PresenceController findMine");
+    } catch (error) {
+      console.log("error at PresenceController findMine");
       console.log(error);
       next(error);
     }
   }
   static async update(req, res, next) {
     try {
-      console.log("try at CompanyController update");
+      console.log("try at PresenceController update");
     } catch (error) {
-      console.log("error at CompanyController update");
+      console.log("error at PresenceController update");
       console.log(error);
       next(error);
     }
   }
   static async delete(req, res, next) {
     try {
-      console.log("try at CompanyController delete");
+      console.log("try at PresenceController delete");
     } catch (error) {
-      console.log("error at CompanyController delete");
+      console.log("error at PresenceController delete");
       console.log(error);
       next(error);
     }
--- server/routes/index.js
+++ server/routes/index.js
@@ -3,7 +3,7 @@ const route = express();
 const OwnerController = require("../controllers/A_OwnerController.js");
 const CompanyController = require("../controllers/B_CompanyController.js");
 const EmployeeController = require("../controllers/C_EmployeeController.js");
-// const PresenceController = require("../controllers/D_PresencesController.js");
+const PresenceController = require("../controllers/D_PresenceController.js");
 
 /*
 mkdir controllers
@@ -17,15 +17,15 @@ route.get("/", function (request, response) {
   response.status(200).json({ message: "Ini backend AmIn (iAmIn)" });
 });
 
-route.post("/owner/register", OwnerController.register);
-route.post("/owner/login", OwnerController.login);
-route.put("/owner/update", OwnerController.update);
+route.post("/owners/register", OwnerController.register);
+route.post("/owners/login", OwnerController.login);
+route.put("/owners/update", OwnerController.update);
 
-route.post("/company/create", CompanyController.create);
-route.get("/company/findOne", CompanyController.findOne);
-route.get("/company/findAll", CompanyController.findAll);
-route.get("/company/findMine", CompanyController.findMine);
-route.put("/company/update", CompanyController.update);
+route.post("/companies/create", CompanyController.create);
+route.get("/companies/findOne", CompanyController.findOne);
+route.get("/companies/findAll", CompanyController.findAll);
+route.get("/companies/findMine", CompanyController.findMine);
+route.put("/companies/update", CompanyController.update);
 
 route.post("/employees/create", EmployeeController.create);
 route.get("/employees/login", EmployeeController.login);
@@ -33,5 +33,12 @@ route.get("/employees/findOne", EmployeeController.findOne);
 route.get("/employees/findAll", EmployeeController.findAll);
 route.get("/employees/findByCompany", EmployeeController.findByCompany);
 route.put("/employees/update", EmployeeController.update);
+
+route.post("/presences/create", PresenceController.create);
+route.get("/presences/findOne", PresenceController.findOne);
+route.get("/presences/findAll", PresenceController.findAll);
+route.get("/presences/findMine", PresenceController.findMine);
+route.put("/presences/update", PresenceController.update);
+route.delete("/presences/update", PresenceController.delete);
 
 module.exports = route;

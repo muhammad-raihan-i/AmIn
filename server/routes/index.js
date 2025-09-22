108d6b036cf85bbd0758d94e1a627340d9812d9d
muhammad-raihan-i <raihan.indrastoto@gmail.com>
2025-09-22 15:47:01 +0700

    chore: routes half done

--- server/routes/index.js
+++ server/routes/index.js
@@ -2,8 +2,8 @@ const express = require("express");
 const route = express();
 const OwnerController = require("../controllers/A_OwnerController.js");
 const CompanyController = require("../controllers/B_CompanyController.js");
-// const EmployeesController = require("../controllers/C_EmployeesController.js");
-// const PresencesController = require("../controllers/D_PresencesController.js");
+const EmployeeController = require("../controllers/C_EmployeeController.js");
+// const PresenceController = require("../controllers/D_PresencesController.js");
 
 /*
 mkdir controllers
@@ -16,9 +16,22 @@ touch controllers/PresencesController.js
 route.get("/", function (request, response) {
   response.status(200).json({ message: "Ini backend AmIn (iAmIn)" });
 });
-//hai aku dev
-route.post("/owner-register", OwnerController.register);
-route.post("/owner-login", OwnerController.login);
-route.put("/owner-update", OwnerController.update);
+
+route.post("/owner/register", OwnerController.register);
+route.post("/owner/login", OwnerController.login);
+route.put("/owner/update", OwnerController.update);
+
+route.post("/company/create", CompanyController.create);
+route.get("/company/findOne", CompanyController.findOne);
+route.get("/company/findAll", CompanyController.findAll);
+route.get("/company/findMine", CompanyController.findMine);
+route.put("/company/update", CompanyController.update);
+
+route.post("/employees/create", EmployeeController.create);
+route.get("/employees/login", EmployeeController.login);
+route.get("/employees/findOne", EmployeeController.findOne);
+route.get("/employees/findAll", EmployeeController.findAll);
+route.get("/employees/findByCompany", EmployeeController.findByCompany);
+route.put("/employees/update", EmployeeController.update);
 
 module.exports = route;

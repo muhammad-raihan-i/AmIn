27b37bc0b6b9358af98fdc6306a48604cf789b18
muhammad-raihan-i <raihan.indrastoto@gmail.com>
2025-09-24 16:07:47 +0700

    chore: company create

--- server/controllers/B_CompanyController.js
+++ server/controllers/B_CompanyController.js
@@ -1,7 +1,10 @@
+const { Company } = require("../models");
 module.exports = class CompanyController {
   static async create(req, res, next) {
     try {
       console.log("try at CompanyController create");
+      const data = await Company.create(req.body);
+      res.status(201).json({ message: "Create success", data });
     } catch (error) {
       console.log("error at CompanyController create");
       console.log(error);

3b7b02a57d074f6e2ceb5affdccaea5792771a30
muhammad-raihan-i <raihan.indrastoto@gmail.com>
2025-09-24 15:01:05 +0700

    chore: owner register done

--- server/controllers/A_OwnerController.js
+++ server/controllers/A_OwnerController.js
@@ -1,7 +1,12 @@
+const { Owner } = require("../models");
+const { sign } = require("../helpers/jwt.js");
 module.exports = class OwnerController {
   static async register(req, res, next) {
     try {
       console.log("try at OwnerController register");
+      const data = await Owner.create(req.body);
+      data.password = undefined;
+      res.status(201).json({ message: "Register success", data });
     } catch (error) {
       console.log("error at OwnerController register");
       console.log(error);
@@ -11,6 +16,7 @@ module.exports = class OwnerController {
   static async login(req, res, next) {
     try {
       console.log("try at OwnerController login");
+      const data = await Owner.findOne({ where: { email: req.body.email } });
     } catch (error) {
       console.log("error at OwnerController login");
       console.log(error);

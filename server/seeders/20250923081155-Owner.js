8a6ce40d73ecc822e730ff93e7b9cbc29bf17a35
muhammad-raihan-i <raihan.indrastoto@gmail.com>
2025-09-24 10:34:27 +0700

    chore: done seeder files

--- server/seeders/20250923081155-Owner.js
+++ server/seeders/20250923081155-Owner.js
@@ -1,9 +1,16 @@
 "use strict";
-const data = require("./data/owners.json");
+let data = require("./data/owners.json");
 const { hash } = require("./helpers/bcrypt.js");
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
+    data = JSON.parse(data);
+    data.map((e) => {
+      e.password = hash(e.password);
+      e.createdAt = new Date();
+      e.updatedAt = new Date();
+      return e;
+    });
     await queryInterface.bulkInsert("Owners", data);
   },
 
